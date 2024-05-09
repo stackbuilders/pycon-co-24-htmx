f85f6e05edccaa1f4e8e894ef585ae8cf8e31e47
CristhianMotoche <cristhian.motoche@gmail.com>
2024-05-08 21:01:30 -0500

    feat: Include country flags

--- app/__main__.py
+++ app/__main__.py
@@ -4,18 +4,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 songs = [
-    { "artist": "Sanalejo", "name": "Diablo" },
-    { "artist": "Les Petits Bâtards", "name": "Señales" },
-    { "artist": "Cuarteto de Nos", "name": "Vida ingrata" },
-    { "artist": "Mägo de Oz", "name": "La Costa del Silencio" },
-    { "artist": "Lolabúm", "name": "Lágrima" },
-    { "artist": "Esteman", "name": "Mr. Trance" },
-    { "artist": "Bersuit Vergarabat", "name": "Señor Cobranza" },
-    { "artist": "Caramelos de Cianuro", "name": "Rubia Sol Morena Luna" },
-    { "artist": "Molotov", "name": "Voto Latino" },
-    { "artist": "Los Bunkers", "name": "Miéntele" },
+    { "artist": "Sanalejo", "name": "Diablo", "country": "🇨🇴" },
+    { "artist": "Les Petits Bâtards", "name": "Señales", "country": "🇪🇨" },
+    { "artist": "Cuarteto de Nos", "name": "Vida ingrata", "country": "🇺🇾" },
+    { "artist": "Mägo de Oz", "name": "La Costa del Silencio", "country": "🇪🇸" },
+    { "artist": "Lolabúm", "name": "Lágrima", "country": "🇪🇨" },
+    { "artist": "Esteman", "name": "Mr. Trance", "country": "🇨🇴" },
+    { "artist": "Bersuit Vergarabat", "name": "Señor Cobranza", "country": "🇦🇷" },
+    { "artist": "Caramelos de Cianuro", "name": "Rubia Sol Morena Luna", "country": "🇻🇪" },
+    { "artist": "Molotov", "name": "Voto Latino", "country": "🇲🇽" },
+    { "artist": "Los Bunkers", "name": "Miéntele", "country": "🇨🇱" },
 ]
 
 @app.get('/')
@@ -26,14 +25,14 @@ def index():
 @app.get('/song')
 def song():
     song = random.choice(songs)
-    return f"<div><p>{song['artist']} - {song['name']}</p></div>"
+    return f"<div><p>{song['artist']} - {song['name']} - {song['country']}</p></div>"
 
 # Web 1.0 - Replaces he whole page!
 
 @app.get('/webone')
 def webone():
     song = random.choice(songs)
-    return render_template('webone.html', song=f"{song['artist']} - {song['name']}")
+    return render_template('webone.html', song=f"{song['artist']} - {song['name']} - {song['country']}")
 
 
 if __name__ == "__main__":
