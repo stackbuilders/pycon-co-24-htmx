a8cdc7eee04105ccb693197b0aa2ad2615bf1aa1
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-13 15:36:04 -0500

    Start backend with `devenv up`

--- devenv.nix
+++ devenv.nix
@@ -1,18 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.git ];
-
-  scripts.hello.exec = "echo hello from $GREET";
-
-  languages.python.enable = true;
-  languages.python.version = "3.12.0";
-  languages.python.venv = {
+  languages.python = {
     enable = true;
-    requirements = ./requirements.txt;
+    version = "3.12.0";
+    venv = {
+      enable = true;
+      requirements = ./requirements.txt;
+    };
   };
 
-  scripts.web.exec = ''
-    python app
-  '';
+  processes.web.exec = "python app";
 }
